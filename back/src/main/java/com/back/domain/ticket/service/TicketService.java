446a72624831768bdeee270607867e47462d21e4
Bin-925 <dogok0925@gmail.com>
2026-06-29 19:15:08 +0900

    Feat: 콘서트 목록/상세 조회 API 구현 #4

--- back/src/main/java/com/back/domain/ticket/service/TicketService.java
+++ back/src/main/java/com/back/domain/ticket/service/TicketService.java
@@ -11,10 +11,12 @@ import com.back.domain.ticket.entity.Ticket;
 import com.back.domain.ticket.repository.TicketRepository;
 import com.back.domain.user.entity.User;
 import com.back.domain.user.repository.UserRepository;
+import com.back.global.exception.ErrorCode;
 import com.back.global.exception.ServiceException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 import java.util.UUID;
 
 @Service
@@ -29,18 +31,18 @@ public class TicketService {
     @Transactional
     public PaymentTicketResponse createTicket(Long userId, PaymentTicketRequest request) {
         User user = userRepository.findById(userId)
-                .orElseThrow(() -> new ServiceException("404-1", "유저가 존재하지 않습니다."));
+                .orElseThrow(() -> exception(ErrorCode.USER_NOT_FOUND));
 
         Schedule schedule = scheduleRepository
                 .findByScheduleIdAndConcert_ConcertId(request.scheduleId(), request.concertId())
-                .orElseThrow(() -> new ServiceException("400-1", "해당 콘서트의 회차가 아닙니다."));
+                .orElseThrow(() -> exception(ErrorCode.INVALID_CONCERT_SCHEDULE));
 
         ScheduleSeat scheduleSeat = scheduleSeatRepository
                 .findWithLockByScheduleIdAndSeatNumber(request.scheduleId(), request.seatNumber())
-                .orElseThrow(() -> new ServiceException("404-2", "해당 좌석이 존재하지 않습니다."));
+                .orElseThrow(() -> exception(ErrorCode.SEAT_NOT_FOUND));
 
         if (scheduleSeat.getSeatStatus() != SeatStatus.HOLD) {
-            throw new ServiceException("400-2", "이미 매진된 좌석입니다.");
+            throw exception(ErrorCode.SEAT_SOLD_OUT);
         }
 
         scheduleSeat.updateSeatStatus(SeatStatus.SOLD_OUT);
@@ -51,7 +53,7 @@ public class TicketService {
                 scheduleSeat,
                 createTicketNumber(),
                 scheduleSeat.getSeatPrice()
-                );
+        );
 
         ticketRepository.save(ticket);
 
@@ -65,14 +67,14 @@ public class TicketService {
                 ticket.isValid()
         );
     }
+
     @Transactional
     public void cancelTicket(Long userId, Long ticketId) {
-
         Ticket ticket = ticketRepository.findByTicketIdAndUser_UserId(ticketId, userId)
-                .orElseThrow(() -> new ServiceException("404-2", "해당 티켓이 존재하지 않습니다."));
+                .orElseThrow(() -> exception(ErrorCode.TICKET_NOT_FOUND_FOR_USER));
 
         if (!ticket.isValid()) {
-            throw new ServiceException("400-3", "이미 취소된 티켓입니다.");
+            throw exception(ErrorCode.TICKET_ALREADY_CANCELLED);
         }
 
         ticket.updateIsValid(false);
@@ -81,5 +83,9 @@ public class TicketService {
 
     public String createTicketNumber() {
         return UUID.randomUUID().toString();
+    }
+
+    private ServiceException exception(ErrorCode errorCode) {
+        return new ServiceException(errorCode);
     }
 }
