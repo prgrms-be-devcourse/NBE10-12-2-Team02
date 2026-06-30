d8a3ce0ad35dfe30d5bca1a3bdf59378b9b3bfb9
bangyewon <byewon1111@gmail.com>
2026-06-30 15:12:58 +0900

    Feat: 특정 회차 좌석 실시간 현황 조회 기능 추가 #45

---
+++ back/src/main/java/com/back/domain/schedule/service/ScheduleService.java
@@ -0,0 +1,39 @@
+package com.back.domain.schedule.service;
+
+import com.back.domain.concert.repository.ConcertRepository;
+import com.back.domain.schedule.dto.ShowScheduleResponse;
+import com.back.domain.schedule.entity.Schedule;
+import com.back.domain.schedule.entity.SeatStatus;
+import com.back.domain.schedule.repository.ScheduleRepository;
+import com.back.domain.schedule.repository.ScheduleSeatRepository;
+import com.back.global.exception.ErrorCode;
+import com.back.global.exception.ServiceException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ScheduleService {
+    private final ScheduleRepository scheduleRepository;
+    private final ConcertRepository concertRepository;
+    private final ScheduleSeatRepository scheduleSeatRepository;
+
+    public ShowScheduleResponse showSchedule(Long concertId,Long scheduleId) {
+
+        if (!concertRepository.existsById(concertId)) {
+            throw new ServiceException(ErrorCode.CONCERT_NOT_FOUND);
+        }
+
+        Schedule schedule = scheduleRepository
+                .findByScheduleIdAndConcert_ConcertId(scheduleId, concertId)
+                .orElseThrow(() -> new ServiceException(ErrorCode.INVALID_CONCERT_SCHEDULE));
+
+        long remainingSeats = scheduleSeatRepository
+                .countBySchedule_ScheduleIdAndSeatStatus(scheduleId, SeatStatus.AVAILABLE);
+
+        return ShowScheduleResponse.from(schedule, remainingSeats);
+    }
+
+}
