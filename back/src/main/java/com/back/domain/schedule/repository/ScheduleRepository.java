aab58d9752f29c2ce1e440ee7102c845f2d1d6c7
Bin-925 <dogok0925@gmail.com>
2026-06-30 09:57:39 +0900

    주석 제거

--- back/src/main/java/com/back/domain/schedule/repository/ScheduleRepository.java
+++ back/src/main/java/com/back/domain/schedule/repository/ScheduleRepository.java
@@ -8,11 +8,9 @@ import java.util.Optional;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 
-    // concertId로 첫 번째 Schedule 조회
     Optional<Schedule> findFirstByConcertConcertId(Long concertId);
 
     List<Schedule> findByConcertConcertId(Long concertId);
 
-    // 팀원 추가: scheduleId + concertId로 조회
     Optional<Schedule> findByScheduleIdAndConcert_ConcertId(Long scheduleId, Long concertId);
 }
