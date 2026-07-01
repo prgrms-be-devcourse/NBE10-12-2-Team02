4e96d0c2e9be7e0eba121e7c65430dab2ff04d93
hj-66 <a01088200534@gmail.com>
2026-07-01 12:40:49 +0900

    Feat: SecurityConfig에 CORS 설정 적용 #54

--- back/src/main/java/com/back/global/config/SecurityConfig.java
+++ back/src/main/java/com/back/global/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
@@ -22,6 +23,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) {
         http
+
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(
                         auth -> auth
                                 .requestMatchers("/h2-console/**").permitAll()
