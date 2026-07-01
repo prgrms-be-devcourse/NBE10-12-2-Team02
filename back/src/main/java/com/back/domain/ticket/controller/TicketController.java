0f7d3701e25f84e2b041cf55e09fc45eb2cb3be7
hj-66 <a01088200534@gmail.com>
2026-07-01 15:29:37 +0900

    Feat: user 패키지 security 적용 및 방식 변경 #63

--- back/src/main/java/com/back/domain/ticket/controller/TicketController.java
+++ back/src/main/java/com/back/domain/ticket/controller/TicketController.java
@@ -4,13 +4,14 @@ import com.back.domain.ticket.dto.PaymentTicketRequest;
 import com.back.domain.ticket.dto.PaymentTicketResponse;
 import com.back.domain.ticket.service.TicketService;
 import com.back.global.annotation.ApiV1;
-import com.back.global.requestcontext.RequestContext;
 import com.back.global.rsData.RsData;
+import com.back.global.security.SecurityUser;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @ApiV1
@@ -21,14 +22,15 @@ import org.springframework.web.bind.annotation.*;
 public class TicketController {
 
     private final TicketService ticketService;
-    private final RequestContext requestContext;
 
     @PostMapping("/reserve")
     @ResponseStatus(HttpStatus.CREATED)
     @Operation(summary = "결제 및 티켓 생성", description = "결제 및 티켓 생성 API")
-    public RsData<PaymentTicketResponse> createTicket(@RequestBody @Valid PaymentTicketRequest request) {
-        Long userId = requestContext.getActor().getUserId();
-        PaymentTicketResponse response = ticketService.createTicket(userId, request);
+    public RsData<PaymentTicketResponse> createTicket(
+            @RequestBody @Valid PaymentTicketRequest request,
+            @AuthenticationPrincipal SecurityUser securityUser
+    ) {
+        PaymentTicketResponse response = ticketService.createTicket(securityUser.getId(), request);
         return new RsData<>(
                 "201-1",
                 "결제 및 티켓 생성 성공",
@@ -38,9 +40,11 @@ public class TicketController {
 
     @PatchMapping("/cancel/{ticketId}")
     @Operation(summary = "결제 취소", description = "결제 취소 API")
-    public RsData<Void> cancelTicket(@PathVariable(value = "ticketId") Long ticketId) {
-        Long userId = requestContext.getActor().getUserId();
-        ticketService.cancelTicket(userId, ticketId);
+    public RsData<Void> cancelTicket(
+            @PathVariable(value = "ticketId") Long ticketId,
+            @AuthenticationPrincipal SecurityUser securityUser
+    ) {
+        ticketService.cancelTicket(securityUser.getId(), ticketId);
         return new RsData<>(
                 "200-1",
                 "티켓 취소 성공",
