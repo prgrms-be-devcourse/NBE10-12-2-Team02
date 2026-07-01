3eb7df7ab3efc82be47261429563b22edb4e0c94
bangyewon <byewon1111@gmail.com>
2026-07-01 17:19:29 +0900

    Feat: bucket4j 기능 추가 #59

--- back/src/main/java/com/back/global/config/WebConfig.java
+++ back/src/main/java/com/back/global/config/WebConfig.java
@@ -1,16 +1,20 @@
 package com.back.global.config;
 
 import com.back.global.annotation.ApiV1;
+import com.back.global.ratelimit.RateLimitInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.HandlerTypePredicate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+    private final RateLimitInterceptor rateLimitInterceptor;
+
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer) {
         configurer.addPathPrefix("/api/v1", HandlerTypePredicate.forAnnotation(ApiV1.class));
@@ -21,5 +25,11 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(rateLimitInterceptor)
+                .addPathPatterns("/api/v1/schedules/*/seats/status");
     }
 }
