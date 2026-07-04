f2ca0bc5bb0cd93db637c30c1bdca04e0253316f
oxcm07 <oxcm0727@gmail.com>
2026-07-04 23:26:48 +0900

    Fix: 중복 선언된 의존성 제거 #115

---
+++ back/src/main/java/com/back/global/config/WebSocketConfig.java
@@ -0,0 +1,31 @@
+package com.back.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+
+        // 1. 클라이언트가 구독 경로
+        registry.enableSimpleBroker("/topic/", "/queue/");
+
+        // 2. 클라이언트 발행 경로
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+
+        //Websocket/stomp 연결 endpoint
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+    }
+}
