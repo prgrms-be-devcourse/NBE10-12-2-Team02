536721cbaf606164a7abeaf67a2e0f4cd8ccc3b2
bangyewon <byewon1111@gmail.com>
2026-07-06 16:46:32 +0900

    Feat: 대기열 순번 조회 기능 추가 #114

--- back/src/main/java/com/back/domain/waiting/controller/WaitingQueueController.java
+++ back/src/main/java/com/back/domain/waiting/controller/WaitingQueueController.java
@@ -5,12 +5,10 @@ import com.back.domain.waiting.service.WaitingQueueService;
 import com.back.global.annotation.ApiV1;
 import com.back.global.requestcontext.RequestContext;
 import com.back.global.rsData.RsData;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @ApiV1
 @RestController
@@ -22,6 +20,7 @@ public class WaitingQueueController {
     private final RequestContext requestContext;
 
     @PostMapping("/concerts/{concertId}/schedules/{scheduleId}/waiting-queue")
+    @Operation(summary = "대기열 등록", description = "대기열 등록 API")
     public RsData<WaitingQueueResponse> registerWaiting(
             @PathVariable Long concertId,
             @PathVariable Long scheduleId
@@ -32,5 +31,20 @@ public class WaitingQueueController {
                 requestContext.getActor().getId()
         );
         return new RsData<>("200-1","대기열 등록 성공",response);
+    }
+
+    @GetMapping("/concerts/{concertId}/schedules/{scheduleId}/waiting-queue/rank")
+    @Operation(summary = "대기열 순번 조회", description = "대기열 순번 조회 API")
+    public RsData<WaitingQueueResponse> showWaitingRank(
+            @PathVariable Long concertId,
+            @PathVariable Long scheduleId
+    ) {
+        WaitingQueueResponse response = waitingQueueService.showWaitingRank(
+                concertId,
+                scheduleId,
+                requestContext.getActor().getId()
+        );
+
+        return new RsData<>("200-1", "대기열 순번 조회 성공", response);
     }
 }
